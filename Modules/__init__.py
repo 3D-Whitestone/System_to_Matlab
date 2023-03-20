3ef5737f7e21ca3e1a747a27b6aea9a5dbf6ca46
Martin <1753172@gmail.com>
2023-03-20 19:02:35 +0100

    adding a init file

---
+++ Modules/__init__.py
@@ -0,0 +1,5 @@
+from .DynamicSymbols import DynamicSymbols
+
+from .Robotik_System import( Robotik_System, Drehmatrix, write_m_func, write_init_prams, diff_t)
+
+__all__ = ['DynamicSymbols', 'Robotik_System', 'Drehmatrix', 'write_m_func', 'write_init_prams', 'diff_t']
